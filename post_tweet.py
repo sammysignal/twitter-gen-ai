d890a57c710e42474ca39267e82c7b85fef76253
Sammy Mehra <sammysignal@gmail.com>
2023-03-11 13:32:51 -0500

    Added logic to ensure only tweeting every so often

--- post_tweet.py
+++ post_tweet.py
@@ -1,4 +1,4 @@
-import os, base64, time, traceback
+import os, base64, time, traceback, pickle
 
 from datetime import datetime
 
@@ -12,15 +12,31 @@ SLEEP_TIME = 21600
 def getTweet():
     return "test"
 
+def cannotTweet():
+    # First we make sure that it has been at least 5.5 hours since our last tweet
+    last_tweet_time = 0
+    try:
+        with open("last_tweet.pkl", "rb") as f:
+            last_tweet_time = pickle.load(f)
+    except EOFError as e:
+        p_logger.error(str(e))
+        return True
+
+    time_diff = time.time() - last_tweet_time
+    if time_diff < (SLEEP_TIME * 0.95):
+        p_logger.error("Cannot tweet now, it has only been " + str(time_diff / 60) + " mins.")
+        return True
+    return False
 
 # Post a tweet then
-def postTweetLoop():
+def postTweetLoop(override=False):
+    if cannotTweet() and not override:
+        return(0)
+
     pj = None
     while True:
         try:
-            now = datetime.now()
-            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-            p_logger.info(dt_string)
+            p_logger.info("######################")
             p_logger.info("Attempting to tweet...")
             tweetBody = getTweet()
             pj = TwitterDriver(U, P)
@@ -28,6 +44,8 @@ def postTweetLoop():
             pj.post_tweet(tweetBody)
             p_logger.info("Tweeted: " + tweetBody)
             pj.quit()
+            with open("last_tweet.pkl","wb") as f:
+                pickle.dump(time.time(), f)
         except Exception as e:
             p_logger.error(e)
             p_logger.error(traceback.format_exc())
@@ -43,5 +61,5 @@ def postTweetLoop():
 
 
 if __name__ == "__main__":
-    exit(postTweetLoop())
+    exit(postTweetLoop(override=True))
     
